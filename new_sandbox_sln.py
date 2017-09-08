bf59c467d422714d47f543f9db548283b08501fe
Voronetskiy Nikolay <voronetskiy.nikolay@gmail.com>
2017-09-08 16:53:51 +0300

    build props

--- new_sandbox_sln.py
+++ new_sandbox_sln.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-import template, sys
+import template, sys, os
 
 print(sys.argv)
 if len(sys.argv) == 2:
     template.unpack_solution("_static_library.zip", "SandBox", sys.argv[1])
+    slnfile = "SandBox\\" + sys.argv[1] + ".sln"
+    os.system("start "+ slnfile)

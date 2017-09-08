2e7af35b94002fd6ddef4a20fa0b110b9338e2c8
Voronetskiy Nikolay <voronetskiy.nikolay@gmail.com>
2017-09-08 15:02:22 +0300

    setup script

--- setup.py
+++ setup.py
@@ -31,12 +31,14 @@ def cwd(new_cwd):
 #
 # Work
 #
-intermediate_path = '_obj-lib-etc'
 
+with cwd('.'):
+    subprocess.check_call(['git', 'submodule', 'update', '--init', '--recursive'])
+
+intermediate_path = '_obj-lib-etc'
 root_path = Path(__file__).parent.resolve()
 build_path = Path(intermediate_path) / 'windows' / 'sharedtec'
 build_path.mkdir(parents=True, exist_ok=True)
-
 with cwd(build_path):
     # See for options help: https://developer.android.com/ndk/guides/cmake.html
     # shutil.rmtree(root_path / 'build')
@@ -50,15 +52,8 @@ with cwd(build_path):
 
     subprocess.check_call(cmd)
 
-with cwd('.'):
-    subprocess.check_call(['git', 'submodule', 'update', '--init', '--recursive'])
-
 with cwd('boost-cmake/boost/boost_1_64_0'):
     p = subprocess.Popen("bootstrap.bat", cwd=r".")
     stdout, stderr = p.communicate()
-    subprocess.check_call(['bjam', '--stagedir="stage/x86"', '-j10', 'toolset=msvc', 'address-model=32', 'variant=release',  'threading=multi', 'link=static', 'runtime-link=static,shared', 'define=_SECURE_SCL=0', 'define=_HAS_ITERATOR_DEBUGGING=0',  'define=BOOST_TEST_NO_MAIN'])
-    subprocess.check_call(['bjam', '--stagedir="stage/x64"', '-j10', 'toolset=msvc', 'address-model=64', 'variant=release',  'threading=multi', 'link=static', 'runtime-link=static,shared', 'define=_SECURE_SCL=0', 'define=_HAS_ITERATOR_DEBUGGING=0',  'define=BOOST_TEST_NO_MAIN'])
-    subprocess.check_call(['bjam', '--stagedir="stage/x86"', '-j10', 'toolset=msvc', 'address-model=32', 'variant=debug'  ,  'threading=multi', 'link=static', 'runtime-link=static,shared', 'define=BOOST_TEST_NO_MAIN'])
-    subprocess.check_call(['bjam', '--stagedir="stage/x64"', '-j10', 'toolset=msvc', 'address-model=64', 'variant=debug'  ,  'threading=multi', 'link=static', 'runtime-link=static,shared', 'define=BOOST_TEST_NO_MAIN'])
-
-print('OK')
+    subprocess.call(['bjam', '--stagedir="stage/x64"', '-j10', 'toolset=msvc', 'address-model=64', 'variant=release',  'threading=multi', 'link=static', 'runtime-link=static,shared', 'define=_SECURE_SCL=0', 'define=_HAS_ITERATOR_DEBUGGING=0',  'define=BOOST_TEST_NO_MAIN'])
+    subprocess.call(['bjam', '--stagedir="stage/x64"', '-j10', 'toolset=msvc', 'address-model=64', 'variant=debug'  ,  'threading=multi', 'link=static', 'runtime-link=static,shared', 'define=BOOST_TEST_NO_MAIN'])

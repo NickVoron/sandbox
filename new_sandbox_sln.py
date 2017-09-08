ee0c00879183d2e02163b1337f9f6f576363f68a
Voronetskiy Nikolay <voronetskiy.nikolay@gmail.com>
2017-09-08 23:56:59 +0300

    build scripts

--- new_sandbox_sln.py
+++ new_sandbox_sln.py
@@ -2,8 +2,13 @@
 
 import template, sys, os
 
+def deploy(solutionName):
+	template.unpack_solution("_static_library.zip", "SandBox", solutionName)
+	slnfile = "SandBox\\" + solutionName + ".sln"
+	os.system("start "+ slnfile)
+
 print(sys.argv)
 if len(sys.argv) == 2:
-    template.unpack_solution("_static_library.zip", "SandBox", sys.argv[1])
-    slnfile = "SandBox\\" + sys.argv[1] + ".sln"
-    os.system("start "+ slnfile)
+	deploy(sys.argv[1])
+elif len(sys.argv) == 1:
+	deploy('HelloLib')
--- setup.py
+++ setup.py
@@ -3,15 +3,7 @@
 import subprocess, contextlib, logging, os, shutil, msbuild
 from pathlib import Path
 
-#
-# Logger
-#
-
 logging.basicConfig(level=logging.INFO)
-
-#
-# Helpers
-#
 
 @contextlib.contextmanager
 def cwd(new_cwd):
@@ -23,39 +15,36 @@ def cwd(new_cwd):
     logging.info('Restore cwd: %s', str(old_cwd))
     os.chdir(str(old_cwd))
 
-#
-# Work
-#
+def setup():
+    with cwd('.'):
+        subprocess.check_call(['git', 'submodule', 'update', '--init', '--recursive'])
 
-with cwd('.'):
-    subprocess.check_call(['git', 'submodule', 'update', '--init', '--recursive'])
+    intermediate_path = '_obj-libs-etc'
+    root_path = Path(__file__).parent.resolve()
+    build_path = Path(intermediate_path) / 'windows' / 'sharedtec'
+    build_path.mkdir(parents=True, exist_ok=True)
+    with cwd(build_path):
+        cmd = [
+            'cmake',
+            '-G', 'Visual Studio 15 2017 Win64',
+            str(root_path) # source dir
+        ]
 
-intermediate_path = '_obj-libs-etc'
-root_path = Path(__file__).parent.resolve()
-build_path = Path(intermediate_path) / 'windows' / 'sharedtec'
-build_path.mkdir(parents=True, exist_ok=True)
-with cwd(build_path):
-    # See for options help: https://developer.android.com/ndk/guides/cmake.html
-    # shutil.rmtree(root_path / 'build')
-    cmd = [
-        'cmake',
-        '-G', 'Visual Studio 15 2017 Win64',
-        str(root_path) # source dir
-    ]
+        logging.info('Run cmake command: %s', str(cmd))
 
-    logging.info('Run cmake command: %s', str(cmd))
+        subprocess.check_call(cmd)
 
-    subprocess.check_call(cmd)
+    with cwd('boost-cmake/boost/boost_1_64_0'):
+        if not os.path.exists("bjam.exe"):
+            p = subprocess.Popen("bootstrap.bat", cwd=r".")
+            stdout, stderr = p.communicate()
+        subprocess.call([r'bjam', r'--stagedir=stage/x64', r'-j10', r'toolset=msvc', r'address-model=64', r'variant=release',  r'threading=multi', r'link=static', r'runtime-link=static,shared', r'define=_SECURE_SCL=0', r'define=_HAS_ITERATOR_DEBUGGING=0',  r'define=BOOST_TEST_NO_MAIN'])
+        subprocess.call([r'bjam', r'--stagedir=stage/x64', r'-j10', r'toolset=msvc', r'address-model=64', r'variant=debug'  ,  r'threading=multi', r'link=static', r'runtime-link=static,shared', r'define=BOOST_TEST_NO_MAIN'])
 
-with cwd('boost-cmake/boost/boost_1_64_0'):
-    if not os.path.exists("bjam.exe"):
-        p = subprocess.Popen("bootstrap.bat", cwd=r".")
-        stdout, stderr = p.communicate()
-    subprocess.call(['bjam', '--stagedir=stage/x64', '-j10', 'toolset=msvc', 'address-model=64', 'variant=release',  'threading=multi', 'link=static', 'runtime-link=static,shared', 'define=_SECURE_SCL=0', 'define=_HAS_ITERATOR_DEBUGGING=0',  'define=BOOST_TEST_NO_MAIN'])
-    subprocess.call(['bjam', '--stagedir=stage/x64', '-j10', 'toolset=msvc', 'address-model=64', 'variant=debug'  ,  'threading=multi', 'link=static', 'runtime-link=static,shared', 'define=BOOST_TEST_NO_MAIN'])
+    with cwd("third_party"):
+        msbuild.build('third_party.sln', 'Release', 'x64')
 
-with cwd("third_party"):
-    subprocess.call([msbuild.msbuild(), 'third_party.sln', '/t:Rebuild', '/p:Configuration=Release', '/maxcpucount:12'])
+    with cwd("Stable"):
+        msbuild.build('libs.sln', 'Release', 'x64')
 
-with cwd("Stable"):
-    subprocess.call([msbuild.msbuild(), 'libs.sln', '/t:Rebuild', '/p:Configuration=Release', '/maxcpucount:12'])
+setup()
